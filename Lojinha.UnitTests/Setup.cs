392184636a0ad18d5ab75239a8a4817239017085
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-12-04 10:16:36 -0300

    Criada classe de configuração do XUnit com injeção de dependências e conexão com o banco.

---
+++ Lojinha.UnitTests/Setup.cs
@@ -0,0 +1,45 @@
+﻿using Lojinha.Domain.Interfaces.Repositories;
+using Lojinha.Domain.Interfaces.Services;
+using Lojinha.Domain.Services;
+using Lojinha.Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Lojinha.UnitTests;
+
+public class Setup : Xunit.Di.Setup
+{
+    protected override void Configure()
+    {
+        ConfigureAppConfiguration((hostingContext, config) =>
+        {
+            #region Ativando a Injeção de dependências no XUnit
+            bool reloadOnChange = hostingContext.Configuration.GetValue("hostBuilder:reloadConfigOnChange", true);
+            if (hostingContext.HostingEnvironment.IsDevelopment())
+                config.AddUserSecrets<Setup>(true, reloadOnChange);
+            #endregion
+        });
+
+        ConfigureServices((context, services) =>
+        {
+            #region Localizando o arquivo appsettings.json
+            var configurationBuilder = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            configurationBuilder.AddJsonFile(path, false);
+            #endregion
+
+            #region Obtendo a connection string do arquivo
+            var root = configurationBuilder.Build();
+            var connectionString = root.GetSection("ConnectionStrings").GetSection("LojinhaDb").Value;
+            #endregion
+
+            #region Realizando a injeção das dependências necessárias
+            services.AddDbContext<SqlServerContext>(options => options.UseSqlServer(connectionString));
+            services.AddTransient<IProdutoRepository, ProdutoRepository>();
+            services.AddTransient<IProdutoDomainService, ProdutoDomainService>();
+            #endregion
+        });
+    }
+}
