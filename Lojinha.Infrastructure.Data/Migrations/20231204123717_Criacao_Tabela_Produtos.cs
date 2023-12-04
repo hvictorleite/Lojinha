64dfab6aa1cd4abbab48ff6916527db6442ac7c3
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-12-04 09:40:14 -0300

    Geradas migrations para a criação da tabela Produtos.

---
+++ Lojinha.Infrastructure.Data/Migrations/20231204123717_Criacao_Tabela_Produtos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lojinha.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Criacao_Tabela_Produtos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    Preco = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Estoque = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_Nome",
+                table: "Produtos",
+                column: "Nome");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
