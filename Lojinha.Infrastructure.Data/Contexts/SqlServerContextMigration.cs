a2db2f10f8c5ddc3413b61481e71cdd1604c6842
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-12-04 08:30:07 -0300

    Criado Context para as Migrations

---
+++ Lojinha.Infrastructure.Data/Contexts/SqlServerContextMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Lojinha.Infrastructure.Data.Contexts;
+
+public class SqlServerContextMigration : IDesignTimeDbContextFactory<SqlServerContext>
+{
+    public SqlServerContext CreateDbContext(string[] args)
+    {
+        #region Lendo o arquivo appsettings.json
+        var configurationBuilder = new ConfigurationBuilder();
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+        configurationBuilder.AddJsonFile(path, false);
+        #endregion
+
+        #region Lendo a connection string do banco no arquivo
+        var root = configurationBuilder.Build();
+        var connectionString = root.GetSection("ConnectionStrings").GetSection("LojinhaDb").Value;
+        #endregion
+
+        #region Fazendo a injeção de dependência na classe SqlServerContext
+        var builder = new DbContextOptionsBuilder<SqlServerContext>();
+        builder.UseSqlServer(connectionString);
+
+        return new SqlServerContext(builder.Options);
+        #endregion
+    }
+}
