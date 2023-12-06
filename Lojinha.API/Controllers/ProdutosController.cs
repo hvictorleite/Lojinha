54e4217ad02223568373862c5338171163127fcc
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-12-06 10:21:29 -0300

    Criado ProdutosController com endpoint para criar produto.

---
+++ Lojinha.API/Controllers/ProdutosController.cs
@@ -0,0 +1,24 @@
+﻿using Lojinha.Application.Commands;
+using Lojinha.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lojinha.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProdutosController : ControllerBase
+{
+    private readonly IProdutoApplicationService _produtoApplicationService;
+
+    public ProdutosController(IProdutoApplicationService produtoApplicationService)
+    {
+        _produtoApplicationService = produtoApplicationService;
+    }
+
+    [HttpPost]
+    public IActionResult CriarProduto([FromBody] CriarProdutoCommand command)
+    {
+        _produtoApplicationService.CriarProduto(command);
+        return StatusCode(StatusCodes.Status201Created, new { message = $"Produto {command.Nome} criado com sucesso." });
+    }
+}
