2ad97ea17cd6c41f48df4e193ab6dc0d5ddd324b
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-12-06 08:54:47 -0300

    Adicionados pacotes AutoMapper, criada classe Command para criar produto e feito mapeamento de command para entidade.

---
+++ Lojinha.Application/Commands/CriarProdutoCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace Lojinha.Application.Commands;
+
+public class CriarProdutoCommand
+{
+    public string? Nome { get; set; }
+    public decimal Preco { get; set; }
+    public int Estoque { get; set; } = 0;
+}
